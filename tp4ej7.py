a1d1e37aeefaffe5779d411fc162434bbcd256f0
diazvaned <83316111+diazvaned@users.noreply.github.com>
2021-06-08 17:28:57 -0300

    Update tp4ej7.py

--- tp4ej7.py
+++ tp4ej7.py
@@ -5,23 +5,17 @@
 ################
 
 import tp4ej1 as soporte
+import tp4ej5 as signos
 
 def division_lenta(dividendo, divisor):
     #quita signos y los guarda
-    signodivisor = 1
-    signodividendo = 1
+    signodivisor = signos.signo(divisor)
+    signodividendo = signos.signo(dividendo)
     cociente = 0
-    if ((dividendo > 0 and divisor > 0) or (dividendo < 0 and divisor < 0)):
-        signodivisor = 1
-        signodividendo = 1
-    elif (dividendo < 0 or divisor < 0):
-        if dividendo <0:
-            dividendo = dividendo * -1
-            signodividendo = -1
-        if divisor < 0:
-            print ("divisor")
-            divisor = divisor * -1
-            signodivisor = -1
+    if dividendo <0:
+        dividendo = dividendo * -1
+    if divisor < 0:
+        divisor = divisor * -1
     #restas sucesivas
     while ((dividendo >= divisor) and (divisor != 0)):
         dividendo = dividendo -divisor
